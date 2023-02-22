69e54a2e9d645d9c7f5fe08264146cf305b9a25a
Aubrey Loudenslager <aubrey.d.webster@gmail.com>
2023-02-22 16:40:05 -0700

    changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+ function superbowlWin (record, sadReality) {
+    const broncos = record.find((record) => record.result === "W")
+    return broncos ? broncos.year : undefined
+ }
+
+
+
